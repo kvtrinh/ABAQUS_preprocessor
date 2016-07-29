d83908b8de787c9b68acfae227dae5c14c3b4cf7
ktrinh <ktrinh@0ed989c4-a85f-429b-80b3-44bdf6cc0fd4>
2016-07-29 17:23:39 +0000

    added

---
+++ GTM/section_extractions/3_surfaces/.DracoLog.py
@@ -0,0 +1,8 @@
+## Draco 8.0.0 (0) Wed Nov 9 14:00:30 PST 2011
+## ACIS 19.0.3  (c) Spatial Corporation
+## Python 2.3.4
+## Qt 4.7.1  (c) Trolltech
+## CAMAL 4.0.1
+## Started by ktrinh on zebra at Fri Jul 29 09:46:13 2016
+
+execfile('Draco_section_extraction_script_temp.py')
---
+++ GTM/section_extractions/3_surfaces/create_GeometryData_from_points_on_curves.m
@@ -0,0 +1,87 @@
+% script to read wing section points from Draco extracted data and create
+% a GeometryData object.
+
+close all;
+clear all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%% inputs %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+scaleFactor = 1.;
+tol = .1;
+spanDirection = 2;             % 
+chordDirection = 1;                  % 
+thicknessDirection = 3;          % 
+
+addpath('/Users/ktrinh/ESAC/SVN/esac/meshing/src/');
+%%%%%%%%%%%%%%%%%% inputs: m file from Draco output %%%%%%%%%%%%%%
+GTM_wing_3s;
+nodes = GTM_wing_3s_data*scaleFactor;
+% fine leading edge data
+
+const_stations = const_stations*scaleFactor;
+
+[r_st c] = size(const_stations);
+constCoords = nodes(:,spanDirection);
+
+numWingSection = 0;
+sections = {};
+figure;
+grid on;
+hold on;
+axis equal;
+plot3(nodes(:,1),nodes(:,2),nodes(:,3),'.k');
+plot3(nodes(:,1),nodes(:,2),nodes(:,3),'b');
+
+completeSections = 20;     % from post-processing
+for i=1:completeSections
+    skipList = [14,15,16,17,18, 19];
+    if find(skipList==i)>0
+        continue;
+    end    
+    curConstCoord = const_stations(i);
+    fprintf('section %d, at %f\n',i,curConstCoord);
+    ids = constCoords < (curConstCoord+tol);
+    curNodes = nodes(ids,:);
+    curConstCoords = curNodes(:,spanDirection);
+    ids = find(curConstCoords > (curConstCoord-tol));
+    curNodes = curNodes(ids,:);    
+    % WingSectionData instantiation
+    curWingSection = WingSectionData();
+    meanThickPercentage = 90.;
+    changeMeanThickList = [14, 16, 18, 20];
+    if find(changeMeanThickList==i)>0
+        meanThickPercentage = 90.;
+    end  
+    curWingSection.populateSectionPoints(curNodes,spanDirection,chordDirection,thicknessDirection,meanThickPercentage);
+    numWingSection = numWingSection + 1;
+    meanU = mean([curWingSection.leadingEdge(spanDirection);...
+        curWingSection.trailingEdge(spanDirection)]);
+    curWingSection.curves(1).u = meanU;
+    curWingSection.curves(2).u = meanU;
+    [nr nc] = size(curWingSection.curves(1).pos);
+    fprintf('section %d, at %f, num nodes on curve 1 %d\n',i,curConstCoord, nr);
+    [nr nc] = size(curWingSection.curves(2).pos);
+    fprintf('section %d, at %f, num nodes on curve 2 %d\n',i,curConstCoord, nr);
+    curWingSection.u = meanU;     
+    curWingSection.wingDataUpdated = 0;
+    sections{i} = curWingSection;
+end
+
+numSection = length(sections);
+
+
+GTM_wing_geom = GeometryData(sections);
+save('GTM_wing_geom', 'GTM_wing_geom');
+g = GTM_wing_geom;
+numSections = length(g.sections);
+for i=1:numSections
+    plt = 0;
+%     checkList = [2;17;20;39];
+%     if find(checkList==i)>0
+%         plt = 1;
+%     end
+    g.sections{i}.checkSection(i,plt);
+end
+g.plot();
+
+
+
